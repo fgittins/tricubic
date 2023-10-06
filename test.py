c4261a57c20bd40a44bf8c6fd1143ac06c148c1d
fgittins <fabiangittins@gmail.com>
2023-10-06 10:53:43 +0100

    Fully pep 8 compliant

--- test.py
+++ test.py
@@ -1,9 +1,10 @@
-__author__  = 'Fabian Gittins'
-__date__    = '15/09/2023'
-
 import unittest
 import numpy as np
 from tricubic import tricubic
+
+__author__ = 'Fabian Gittins'
+__date__ = '06/10/2023'
+
 
 class Test(unittest.TestCase):
     rng = np.random.default_rng(15092023)
@@ -38,14 +39,15 @@ class Test(unittest.TestCase):
                     self.assertAlmostEqual(f(x, y, z), F[i, j, k])
 
     def test_linear(self):
-        ftest = lambda x, y, z: 1 + 2*x + 3*y - 4*z
+        def ftest(x, y, z):
+            return 1 + 2*x + 3*y - 4*z
 
         n = 11
         X = Y = Z = np.linspace(-5, 5, n)
         F = [[[ftest(x, y, z) for z in Z] for y in Y] for x in X]
 
         f = tricubic(X, Y, Z, F)
-        
+
         # focus on patch in grid
         Xtest = Ytest = Ztest = np.linspace(0, 1, 3*n)
         for x in Xtest:
@@ -57,7 +59,8 @@ class Test(unittest.TestCase):
                     self.assertAlmostEqual(f(x, y, z, dz=True), -4)
 
     def test_cubic(self):
-        ftest = lambda x, y, z: -10 + 0.1*x - 2*x**2 + x**3 - 5*y**2
+        def ftest(x, y, z):
+            return -10 + 0.1*x - 2*x**2 + x**3 - 5*y**2
 
         n = 11
         X = Y = Z = np.linspace(-5, 5, n)
@@ -71,13 +74,14 @@ class Test(unittest.TestCase):
             for y in Ytest:
                 for z in Ztest:
                     self.assertAlmostEqual(f(x, y, z), ftest(x, y, z))
-                    self.assertAlmostEqual(f(x, y, z, dx=True), 
+                    self.assertAlmostEqual(f(x, y, z, dx=True),
                                            0.1 - 4*x + 3*x**2)
                     self.assertAlmostEqual(f(x, y, z, dy=True), -10*y)
                     self.assertAlmostEqual(f(x, y, z, dz=True), 0)
 
     def test_ricker_wavelet_like(self):
-        ftest = lambda x, y, z: x*np.exp(- x**2 - y**2 - z**2)
+        def ftest(x, y, z):
+            return x*np.exp(- x**2 - y**2 - z**2)
 
         n = 51
         X = Y = Z = np.linspace(-2, 2, n)
@@ -87,14 +91,14 @@ class Test(unittest.TestCase):
 
         x, y, z = self.rng.random(3,)
         self.assertAlmostEqual(f(x, y, z), ftest(x, y, z), places=5)
-        self.assertAlmostEqual(f(x, y, z, dx=True), 
-                               (1 - 2*x**2)*np.exp(- x**2 - y**2 - z**2), 
+        self.assertAlmostEqual(f(x, y, z, dx=True),
+                               (1 - 2*x**2)*np.exp(- x**2 - y**2 - z**2),
                                places=3)
-        self.assertAlmostEqual(f(x, y, z, dy=True), 
+        self.assertAlmostEqual(f(x, y, z, dy=True),
                                -2*x*y*np.exp(- x**2 - y**2 - z**2), places=3)
-        self.assertAlmostEqual(f(x, y, z, dz=True), 
+        self.assertAlmostEqual(f(x, y, z, dz=True),
                                -2*x*z*np.exp(- x**2 - y**2 - z**2), places=3)
+
 
 if __name__ == '__main__':
     unittest.main()
-    
